10396c6cc9654339196a4681373f1fc1a4c91db5
ila76597-cmd <ila76597@gmail.com>
2025-08-19 22:20:57 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+YOOKASSA_SHOP_ID = os.getenv("YOOKASSA_SHOP_ID")
+YOOKASSA_SECRET_KEY = os.getenv("YOOKASSA_SECRET_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
